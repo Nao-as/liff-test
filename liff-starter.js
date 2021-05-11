f2a90203cc12ef5d801384142b18175c968400ed
as-Nao <alfo.gina9110@gmail.com>
2021-05-11 15:32:02 +0900

    修正

--- liff-starter.js
+++ liff-starter.js
@@ -16,7 +16,6 @@ function initializeApp(data) {
     document.getElementById('openwindowbutton').addEventListener('click', function () {
         liff.openWindow({
             // url: 'https://line.me'
-            // url : 'https://nao-liff-test.herokuapp.com/src/test1.html'
             url : 'https://nao-liff-test.herokuapp.com/src/test1.html',
             external: false
         });
